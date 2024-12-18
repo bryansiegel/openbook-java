53a809d9e8998edddb654a42d0efbcd26b4f9b40
Bryan Siegel <bryansiegel@gmail.com>
2024-12-18 14:53:28 -0800

    dashboard skeleton design

--- src/main/java/com/bryansiegel/openbookjava/config/WebSecurityConfig.java
+++ src/main/java/com/bryansiegel/openbookjava/config/WebSecurityConfig.java
@@ -18,7 +18,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/login").permitAll()
+                        .requestMatchers("/", "/login", "/css/*", "/js/*").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
