48670d95bfe70f6ddd5a05bd0bfce367b1664361
Bryan Siegel <bryansiegel@gmail.com>
2024-12-18 11:47:15 -0800

    home controller

---
+++ src/main/java/com/bryansiegel/openbookjava/OpenbookJavaApplication.java
@@ -0,0 +1,13 @@
+package com.bryansiegel.openbookjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenbookJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenbookJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bryansiegel/openbookjava/OpenbookJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bryansiegel.openbookjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenbookJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
