6aa6b2ad8a5f3d583e5a75c55eb5cffbae65c703
Bryan Siegel <bryansiegel@gmail.com>
2024-12-19 10:52:33 -0800

    return username to admin dashboard

--- src/main/java/com/bryansiegel/openbookjava/config/SecurityConfig.java
+++ src/main/java/com/bryansiegel/openbookjava/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,7 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 
-public class WebSecurityConfig {
+public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -27,7 +28,7 @@ public class WebSecurityConfig {
                         .permitAll()
                         .defaultSuccessUrl("/admin/dashboard")
                 )
-                .logout((logout) -> logout.permitAll());
+                .logout(LogoutConfigurer::permitAll);
 
         return http.build();
     }
--- src/main/java/com/bryansiegel/openbookjava/controllers/AuthController.java
+++ src/main/java/com/bryansiegel/openbookjava/controllers/AuthController.java
@@ -1,6 +1,8 @@
 package com.bryansiegel.openbookjava.controllers;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -13,7 +15,8 @@ public class AuthController {
     }
 
     @GetMapping("admin/dashboard")
-    public String dashboard() {
+    public String dashboard(Authentication authentication, Model model) {
+        model.addAttribute("user", authentication.getName());
         return "admin/dashboard";
     }
 }
