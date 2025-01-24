2435d79997beb6ff653c726548578ace90b29fae
Bryan Siegel <bryansiegel@gmail.com>
2025-01-24 08:52:16 -0800

    Elementary Schools controller connect to repo

--- src/main/java/com/bryansiegel/openbookjava/controllers/AuthController.java
+++ src/main/java/com/bryansiegel/openbookjava/controllers/AuthController.java
@@ -8,19 +8,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AuthController {
 
-    @GetMapping("/login/")
+    @GetMapping("/login")
     public String login() {
-        return "admin/login/";
+        return "admin/login";
     }
 
     @GetMapping("/login-error/")
     public String loginError() {
-        return "admin/login-error/";
+        return "admin/login-error";
     }
 
     @GetMapping("admin/dashboard/")
     public String dashboard(Authentication authentication, Model model) {
         model.addAttribute("user", authentication.getName());
-        return "admin/dashboard/";
+        return "admin/dashboard";
     }
 }
--- src/main/java/com/bryansiegel/openbookjava/controllers/ElementarySchoolsController.java
+++ src/main/java/com/bryansiegel/openbookjava/controllers/ElementarySchoolsController.java
@@ -1,43 +1,57 @@
 package com.bryansiegel.openbookjava.controllers;
 
+import com.bryansiegel.openbookjava.models.ElementarySchools;
 import com.bryansiegel.openbookjava.repositories.ElementarySchoolsRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ElementarySchoolsController {
 
+    private final ElementarySchoolsRepo elementarySchoolsRepo;
+
+    @Autowired
     public ElementarySchoolsController(ElementarySchoolsRepo elementarySchoolsRepo) {
-        ElementarySchoolsRepo elementarySchoolsRepo1 = elementarySchoolsRepo;
+        this.elementarySchoolsRepo = elementarySchoolsRepo;
     }
 
     @GetMapping("/admin/elementary-schools/")
-    public String index() {
+    public String index(ElementarySchoolsRepo elementarySchoolsRepo, Model model) {
+        model.addAttribute("elementarySchools", elementarySchoolsRepo.findAll());
         return "admin/ElementarySchools/index";
     }
 
     @GetMapping("/admin/elementary-schools/create")
-    public String create() {
+    public String create(ElementarySchoolsRepo elementarySchoolsRepo, Model model) {
+        model.addAttribute("elementarySchools", elementarySchoolsRepo.save(new ElementarySchools()));
         return "admin/ElementarySchools/create";
     }
 
-    @GetMapping("/admin/elementary-schools/store")
-    public String store() {
+    @PostMapping("/admin/elementary-schools/store")
+    public String store(ElementarySchoolsRepo elementarySchoolsRepo, Model model) {
+        model.addAttribute("elementarySchools", elementarySchoolsRepo.save(new ElementarySchools()));
         return "admin/ElementarySchools/store";
     }
 
     @GetMapping("/admin/elementary-schools/edit/{id}")
-    public String edit(int id) {
+    public String edit(@PathVariable Long id, ElementarySchoolsRepo elementarySchoolsRepo, Model model) {
+        model.addAttribute("elementarySchools", elementarySchoolsRepo.findById(id));
         return "admin/ElementarySchools/edit/{id}";
     }
 
     @GetMapping("/admin/elementary-schools/show/{id}")
-    public String show(int id) {
+    public String show(@PathVariable Long id, ElementarySchoolsRepo elementarySchoolsRepo, Model model) {
+        Model elementarySchools = model.addAttribute("elementarySchools", elementarySchoolsRepo.findById(id));
         return "admin/ElementarySchools/show/{id}";
     }
 
     @GetMapping("/admin/elementary-schools/delete/{id}")
-    public String delete(int id) {
+    public String delete(@PathVariable Long id, ElementarySchoolsRepo elementarySchoolsRepo, Model model) {
+        Model elementarySchools = model.addAttribute("elementarySchools", elementarySchoolsRepo.findById(id));
         return "admin/ElementarySchools/delete/{id}";
     }
 
