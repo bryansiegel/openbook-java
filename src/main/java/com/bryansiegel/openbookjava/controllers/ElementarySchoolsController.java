0c94adccc8778d23d667fdf6d6338f45bf3f711f
Bryan Siegel <bryansiegel@gmail.com>
2025-01-29 07:58:36 -0800

    added username to controllers

--- src/main/java/com/bryansiegel/openbookjava/controllers/ElementarySchoolsController.java
+++ src/main/java/com/bryansiegel/openbookjava/controllers/ElementarySchoolsController.java
@@ -3,6 +3,7 @@ package com.bryansiegel.openbookjava.controllers;
 import com.bryansiegel.openbookjava.models.ElementarySchools;
 import com.bryansiegel.openbookjava.repositories.ElementarySchoolsRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,29 +13,35 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class ElementarySchoolsController {
 
+
     private final ElementarySchoolsRepo elementarySchoolsRepo;
+
+
 
     @Autowired
     public ElementarySchoolsController(ElementarySchoolsRepo elementarySchoolsRepo) {
         this.elementarySchoolsRepo = elementarySchoolsRepo;
     }
 
+
     //admin/index
     @GetMapping("/admin/elementary-schools/")
-    public String index( Model model) {
+    public String index(Authentication authentication, Model model) {
         model.addAttribute("elementarySchools", elementarySchoolsRepo.findAll());
+        model.addAttribute("user", authentication.getName());
         return "admin/ElementarySchools/index";
     }
 
     //admin/create
     @GetMapping("/admin/elementary-schools/create")
-    public String create(ElementarySchoolsRepo elementarySchoolsRepo, Model model) {
+    public String create(ElementarySchoolsRepo elementarySchoolsRepo, Authentication authentication,Model model) {
         model.addAttribute("elementarySchools", elementarySchoolsRepo);
+        model.addAttribute("user", authentication.getName());
         return "admin/ElementarySchools/create";
     }
 
-    //admin/create
-    @PostMapping("/admin/elementary-schools/create")
+    //admin/store
+    @PostMapping("/admin/elementary-schools/store")
     public String saveElementarySchool(ElementarySchools elementarySchools) {
         elementarySchoolsRepo.save(elementarySchools);
         return "redirect:/admin/elementary-schools/";
@@ -42,17 +49,19 @@ public class ElementarySchoolsController {
 
     //admin/edit
     @GetMapping("/admin/elementary-schools/edit/{id}")
-    public String edit(@PathVariable Long id, Model model) {
+    public String edit(@PathVariable Long id, Authentication authentication, Model model) {
         ElementarySchools elementarySchools = elementarySchoolsRepo.findById(id).orElse(null);
         model.addAttribute("elementarySchools", elementarySchools);
+        model.addAttribute("user", authentication.getName());
         return "admin/ElementarySchools/edit";
     }
 
     //admin/show
     @GetMapping("/admin/elementary-schools/show/{id}")
-    public String show(@PathVariable Long id, Model model) {
+    public String show(@PathVariable Long id, Authentication authentication, Model model) {
         ElementarySchools elementarySchools = elementarySchoolsRepo.findById(id).orElse(null);
         model.addAttribute("elementarySchools", elementarySchools);
+        model.addAttribute("user", authentication.getName());
 
         return "/admin/ElementarySchools/show";
     }
