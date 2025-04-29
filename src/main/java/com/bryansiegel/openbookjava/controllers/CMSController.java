58df85d2480f43792a6e51d2180c235ee302f955
Bryan Siegel <bryansiegel@gmail.com>
2025-04-29 08:25:07 -0700

    cms

---
+++ src/main/java/com/bryansiegel/openbookjava/controllers/CMSController.java
@@ -0,0 +1,26 @@
+package com.bryansiegel.openbookjava.controllers;
+
+import com.bryansiegel.openbookjava.repositories.CMSRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CMSController {
+
+
+    private final CMSRepo cmsRepo;
+
+    @Autowired
+    public CMSController(CMSRepo cmsRepo) {
+        this.cmsRepo = cmsRepo;
+    }
+
+@GetMapping("admin/cms/")
+    private String index(Model model) {
+        model.addAttribute("cms", cmsRepo.findAll());
+        return "admin/CMS/index";
+    }
+
+}
