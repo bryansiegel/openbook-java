1eedf8c4c5b2c8e35c63951cf053fdd25fe8b924
Bryan Siegel <bryansiegel@gmail.com>
2025-05-01 13:54:46 -0700

    dashboard home

--- src/main/java/com/bryansiegel/openbookjava/controllers/AuthController.java
+++ src/main/java/com/bryansiegel/openbookjava/controllers/AuthController.java
@@ -1,5 +1,6 @@
 package com.bryansiegel.openbookjava.controllers;
 
+import com.bryansiegel.openbookjava.repositories.ElementarySchoolsRepo;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthController {
+
+    private final ElementarySchoolsRepo elementarySchoolsRepo;
+
+    public AuthController(ElementarySchoolsRepo elementarySchoolsRepo) {
+        this.elementarySchoolsRepo = elementarySchoolsRepo;
+    }
 
     @GetMapping("/login")
     public String login() {
@@ -21,6 +28,7 @@ public class AuthController {
     @GetMapping("admin/dashboard/")
     public String dashboard(Authentication authentication, Model model) {
         model.addAttribute("user", authentication.getName());
+        model.addAttribute("elementarySchools", elementarySchoolsRepo.findAll());
 
         return "admin/dashboard";
     }
